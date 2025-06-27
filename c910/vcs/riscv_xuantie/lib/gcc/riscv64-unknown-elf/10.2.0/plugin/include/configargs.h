c238cf64f6cf7aa0f651c689779f4454ebc44434
zhe jiang <zhejiang.uk@gmail.com>
2025-06-27 13:15:35 +0800

    [Initalisation] Added VCS for C910.

---
+++ c910/vcs/riscv_xuantie/lib/gcc/riscv64-unknown-elf/10.2.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "/home/yangkai/1_SA/1_Verilator/openc910/xuantie-gnu-toolchain/riscv-gcc/configure --target=riscv64-unknown-elf --prefix=/software/riscv_xuantie --disable-shared --disable-threads --enable-languages=c,c++ --with-system-zlib --enable-tls --with-newlib --with-sysroot=/software/riscv_xuantie/riscv64-unknown-elf --with-native-system-header-dir=/include --disable-libmudflap --disable-libssp --disable-libquadmath --disable-libgomp --disable-nls --disable-tm-clone-registry --src=.././riscv-gcc --disable-multilib --with-abi=lp64d --with-arch=rv64imafdc 'CFLAGS_FOR_TARGET=-Os   -mcmodel=medlow' 'CXXFLAGS_FOR_TARGET=-Os   -mcmodel=medlow'";
+static const char thread_model[] = "single";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "abi", "lp64d" }, { "arch", "rv64imafdc" } };
---
+++ c910/vcs/riscv_xuantie/lib/gcc/riscv64-unknown-linux-gnu/10.2.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "/home/yangkai/1_SA/1_Verilator/openc910/xuantie-gnu-toolchain/riscv-gcc/configure --target=riscv64-unknown-linux-gnu --prefix=/software/riscv_xuantie --with-sysroot=/software/riscv_xuantie/sysroot --with-system-zlib --enable-shared --enable-tls --enable-languages=c,c++,fortran --disable-libmudflap --disable-libssp --disable-libquadmath --disable-libsanitizer --disable-nls --disable-bootstrap --src=.././riscv-gcc --disable-multilib --with-abi=lp64d --with-arch=rv64imafdc 'CFLAGS_FOR_TARGET=-O2   -mcmodel=medlow' 'CXXFLAGS_FOR_TARGET=-O2   -mcmodel=medlow'";
+static const char thread_model[] = "posix";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "abi", "lp64d" }, { "arch", "rv64imafdc" } };
